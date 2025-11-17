925b79acccd4a5b8e869ad319cebdbe3edbd1f4b
Adam Baty <adambaty100@gmail.com>
2025-11-17 08:03:58 -0500

    Add stat card component

--- knightime-ffl-frontend/src/app/app.ts
+++ knightime-ffl-frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TeamRecordsComponent } from './team-records/team-records';
+import { StatCardComponent } from './stat-card/stat-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TeamRecordsComponent],
+  imports: [RouterOutlet, TeamRecordsComponent, StatCardComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ knightime-ffl-frontend/src/app/stat-card/stat-card.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-stat-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stat-card.html',
+  styleUrl: './stat-card.css'
+})
+export class StatCardComponent {
+  @Input() label: string = '';
+  @Input() value: string | number = 0;
+  @Input() change: string = '';
+  @Input() isPositive: boolean = false;
+}
